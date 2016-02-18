7d72a249fececab70b10efa775e1409c334aae2e
Liran Sharir <lsharir@gmail.com>
2016-02-18 09:28:44 +0200

    blank app

--- src/components/app.ts
+++ src/components/app.ts
@@ -1,10 +1,7 @@
 import { Component } from 'angular2/core';
-import {AwesomeComponent} from "./awesome-component/awesome-component";
 
 @Component({
-    selector: 'my-app',
-    directives: [AwesomeComponent],
-    template: '<awesome-component></awesome-component>'
+    selector: 'my-app'
 })
 
 export class App {
